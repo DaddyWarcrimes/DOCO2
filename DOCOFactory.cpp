3e08929352900d2202b7ecdfb527740b6ca17062
DaddyWarcrimes <4444646+DaddyWarcrimes@users.noreply.github.com>
2022-04-08 21:26:20 -0500

    Consolidated vertical behavior to single instance

--- DOCOFactory.cpp
+++ DOCOFactory.cpp
@@ -51,14 +51,13 @@ void DOCOFactory::populateList(tenantlist* clist, map* myMap, std::string file)
 {
 	char direction[10];
 	int u,v; 
-
+	behavior* addBeh = new verticalBehavior();
 	DataParser* dp = dp->getInstance(file.c_str());
 	while(dp->getDOCOData(direction,&u,&v) )
 	{
 		if (direction[0] == 'V')
 		{
 		tenant* addition = new DOCO(myMap, u,v,STARTENERGY);
-		behavior* addBeh = new verticalBehavior();
 		addition->setBehavior(addBeh);
 		clist->addTenant(addition);
 		}
